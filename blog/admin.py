34f1eea7ef13b28206c0b7432e78c6ba864a0db7
skokado <terutacchi@gmail.com>
2021-04-12 16:01:42 +0900

    実装箇所が誤っていたので修正

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from account.models import User
+from blog.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author')
+    list_display_links = ('id', 'title')
+    fieldsets = (
+        (None, {'fields': ('title', 'body')}),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        return super().save_model(request, obj, form, change)
